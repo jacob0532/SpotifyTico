d1d839c5de6e548fa16a3b423f3087f2fe6798d9
Ary2000 <aryelduranballestero@gmail.com>
2021-08-21 21:29:33 -0600

    Biografia, discografia, genre, mood y style usados, con fotos

---
+++ Página/cargarInfo.js
@@ -0,0 +1,4 @@
+const datosArtistaNombre = JSON.parse(localStorage.getItem('datos-artista'));
+const datosArtistaId = JSON.parse(localStorage.getItem('datos-artistaId'));
+const datosDiscografia = JSON.parse(localStorage.getItem('datos-discografia'));
+const datosAlbumes = JSON.parse(localStorage.getItem('datos-albumes'));
